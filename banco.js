1c0943ef4b9dc1147b9f8089cce984cd834de6ed
Ruan Pablo <ruanapoc@gmail.com>
2025-04-06 20:13:54 -0300

    primeiro commit - login EduStream

---
+++ banco.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql2/promise'); 
+
+async function conectarBD()
+{ 
+    if(global.conexao && global.conexao.state !== 'disconnected') 
+    {
+        return global.conexao;
+    }
+    
+    /*
+    const connectionString = 'mysql://root:senha@localhost:3306/livraria' 
+    const connection= await mysql.createConnection(connectionString)
+    */
+    
+    const conexao = await mysql.createConnection(
+        { 
+            host     : 'localhost', 
+            port     : 3306, 
+            user     : 'root',
+            password : '', 
+            database : 'edustream' 
+        }); 
+        
+    console.log('Conectou no MySQL!'); 
+
+    global.conexao = conexao; 
+    return global.conexao; 
+} 
+
+
+async function buscarUsuario(usuario)
+{
+    const conexao = await conectarBD();
+    const sql = "select * from usuario where emailUsuario=? and senhaUsuario=?;";
+    const [usuarioEcontrado] = await conexao.query(sql,[usuario.email, usuario.senha]);
+    return usuarioEcontrado && usuarioEcontrado.length>0 ? usuarioEcontrado[0] : {};
+}
+
+conectarBD()
+
+module.exports = { buscarUsuario }
---
+++ routes/index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+
+
+/*
+ * Rotas GET de usuários
+*/
+
+/* GET login page */
+router.get('/', function(req, res, next) {
+  if (global.usuarioEmail && global.usuarioEmail != "") {
+    res.redirect('home');
+  }
+
+  res.render('index', { titulo: 'EduStream - Login e Cadastro' });
+});
+
+
+/* GET home page */
+router.get('/home', function(req, res, next) {
+  verificarLogin(res);
+  res.render('home', { titulo: 'EduStream - Home' });
+});
+
+/**
+ * Rotas POST de usuários
+*/
+
+/* POST login */
+
+const path = require('path'); // Só se ainda não tiver no topo
+
+/* GET login em HTML puro */
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/login.html'));
+});
+
+router.post('/login', async function(req, res, next){
+  const email = req.body.email ;
+  const senha = req.body.senha;
+
+  const usuario = await global.banco.buscarUsuario({email,senha});
+
+  global.usuarioCodigo = usuario.idUsuario;
+  global.usuarioEmail = usuario.emailUsuario;
+  res.redirect('home');
+})
+
+
+
+/**
+ * 
+ * Funções diversas
+ * 
+ */
+
+function verificarLogin(res)
+{
+  if (!global.usuarioEmail || global.usuarioEmail == "")
+    res.redirect('/');
+}
+
+
+
+module.exports = router;
